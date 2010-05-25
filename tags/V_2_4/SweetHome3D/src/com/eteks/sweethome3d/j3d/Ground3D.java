921c35ecd92f6530d9d5d03f7200a93f8e41b9a1
nobody <nobody@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2010-05-25 16:06:06 +0000

    This commit was manufactured by cvs2svn to create tag 'V_2_4'.

---
+++ tags/V_2_4/SweetHome3D/src/com/eteks/sweethome3d/j3d/Ground3D.java
@@ -0,0 +1,140 @@
+/*
+ * Ground3D.java 23 janv. 2009
+ *
+ * Copyright (c) 2009 Emmanuel PUYBARET / eTeks <info@eteks.com>. All Rights Reserved.
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package com.eteks.sweethome3d.j3d;
+
+import java.awt.Color;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.media.j3d.Appearance;
+import javax.media.j3d.ColoringAttributes;
+import javax.media.j3d.Shape3D;
+import javax.media.j3d.Texture;
+import javax.vecmath.Color3f;
+import javax.vecmath.Point3f;
+import javax.vecmath.TexCoord2f;
+
+import com.eteks.sweethome3d.model.Home;
+import com.eteks.sweethome3d.model.HomeTexture;
+import com.sun.j3d.utils.geometry.GeometryInfo;
+
+/**
+ * Root of a the 3D ground.
+ * @author Emmanuel Puybaret
+ */
+public class Ground3D extends Object3DBranch {
+  private final float originX;
+  private final float originY;
+  private final float width;
+  private final float depth;
+
+  /**
+   * Creates a 3D ground for the given <code>home</code>.
+   */
+  public Ground3D(Home home,
+                  float groundOriginX,
+                  float groundOriginY,
+                  float groundWidth,
+                  float groundDepth, 
+                  boolean waitTextureLoadingEnd) {
+    setUserData(home);
+    this.originX = groundOriginX;
+    this.originY = groundOriginY;
+    this.width = groundWidth;
+    this.depth = groundDepth;
+
+    // Use coloring attributes for ground to avoid ground lighting
+    ColoringAttributes groundColoringAttributes = new ColoringAttributes();
+    groundColoringAttributes.setCapability(ColoringAttributes.ALLOW_COLOR_WRITE);
+    
+    Appearance groundAppearance = new Appearance();
+    groundAppearance.setColoringAttributes(groundColoringAttributes);
+    groundAppearance.setCapability(Appearance.ALLOW_COLORING_ATTRIBUTES_READ);
+    groundAppearance.setCapability(Appearance.ALLOW_TEXTURE_WRITE);
+
+    final Shape3D groundShape = new Shape3D();
+    groundShape.setAppearance(groundAppearance);
+    groundShape.setCapability(Shape3D.ALLOW_GEOMETRY_WRITE);
+    groundShape.setCapability(Shape3D.ALLOW_APPEARANCE_READ);
+    
+    setCapability(ALLOW_CHILDREN_READ);
+    
+    addChild(groundShape);
+
+    update(waitTextureLoadingEnd);    
+  }
+  
+  /**
+   * Updates ground coloring and texture attributes from home ground color and texture.
+   */
+  @Override
+  public void update() {
+    update(false);
+  }
+  
+  /**
+   * Updates ground coloring and texture attributes from home ground color and texture.
+   */
+  private void update(boolean waitTextureLoadingEnd) {
+    Home home = (Home)getUserData();
+    Shape3D groundShape = (Shape3D)getChild(0);
+    
+    Color3f groundColor = new Color3f(new Color(home.getEnvironment().getGroundColor()));
+    final Appearance groundAppearance = groundShape.getAppearance();
+    groundAppearance.getColoringAttributes().setColor(groundColor);
+    HomeTexture groundTexture = home.getEnvironment().getGroundTexture();
+    if (groundTexture != null) {
+      final TextureManager imageManager = TextureManager.getInstance();
+      imageManager.loadTexture(groundTexture.getImage(), waitTextureLoadingEnd,
+          new TextureManager.TextureObserver() {
+              public void textureUpdated(Texture texture) {
+                groundAppearance.setTexture(texture);
+              }
+            });
+    } else {
+      groundAppearance.setTexture(null);
+    }
+    
+    // Create ground geometry
+    float groundOffset = -0.05f;
+    List<Point3f> coords = new ArrayList<Point3f>();
+    // First add the coordinates of the ground rectangle
+    coords.add(new Point3f(this.originX, groundOffset, this.originY)); 
+    coords.add(new Point3f(this.originX, groundOffset, this.originY + this.depth));
+    coords.add(new Point3f(this.originX + this.width, groundOffset, this.originY + this.depth));
+    coords.add(new Point3f(this.originX + this.width, groundOffset, this.originY));
+    // Compute ground texture coordinates if necessary
+    List<TexCoord2f> textureCoords = new ArrayList<TexCoord2f>();
+    if (groundTexture != null) {
+      textureCoords.add(new TexCoord2f(0, 0));
+      textureCoords.add(new TexCoord2f(0, -this.depth / groundTexture.getHeight()));
+      textureCoords.add(new TexCoord2f(this.width / groundTexture.getWidth(), -this.depth / groundTexture.getHeight()));
+      textureCoords.add(new TexCoord2f(this.width / groundTexture.getWidth(), 0));
+    }
+    GeometryInfo geometryInfo = new GeometryInfo(GeometryInfo.QUAD_ARRAY);
+    geometryInfo.setCoordinates (coords.toArray(new Point3f [coords.size()]));
+    if (groundTexture != null) {
+      geometryInfo.setTextureCoordinateParams(1, 2);
+      geometryInfo.setTextureCoordinates(0, textureCoords.toArray(new TexCoord2f [textureCoords.size()]));
+    }
+    
+    groundShape.setGeometry(geometryInfo.getIndexedGeometryArray());
+  }
+}
