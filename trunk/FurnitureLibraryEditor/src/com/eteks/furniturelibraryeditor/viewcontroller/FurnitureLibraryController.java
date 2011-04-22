5da3b8e7a39372ead6308026055381fdf020bc40
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2011-04-22 17:37:13 +0000

    Updated copyright notice

--- trunk/FurnitureLibraryEditor/src/com/eteks/furniturelibraryeditor/viewcontroller/FurnitureLibraryController.java
+++ trunk/FurnitureLibraryEditor/src/com/eteks/furniturelibraryeditor/viewcontroller/FurnitureLibraryController.java
@@ -1,7 +1,7 @@
 /*
  * FurnitureLibraryController.java 15 mai 2006
  *
- * Copyright (c) 2006 Emmanuel PUYBARET / eTeks <info@eteks.com>. All Rights Reserved.
+ * Furniture Library Editor, Copyright (c) 2006 Emmanuel PUYBARET / eTeks <info@eteks.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
--- trunk/FurnitureLibraryEditor/src/com/eteks/furniturelibraryeditor/viewcontroller/ImportFurnitureController.java
+++ trunk/FurnitureLibraryEditor/src/com/eteks/furniturelibraryeditor/viewcontroller/ImportFurnitureController.java
@@ -1,7 +1,7 @@
 /*
  * ImportFurnitureController.java 13 janv. 2010
  *
- * Copyright (c) 2010 Emmanuel PUYBARET / eTeks <info@eteks.com>. All Rights Reserved.
+ * Furniture Library Editor, Copyright (c) 2010 Emmanuel PUYBARET / eTeks <info@eteks.com>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
