fa22c504b3623e0b732ffffd1594a3b42bb9b65d
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2012-12-01 16:29:18 +0000

    Simplified ViewFactory reuse with ViewFactoryAdapter

--- trunk/FurnitureLibraryEditor/src/com/eteks/furniturelibraryeditor/viewcontroller/ImportFurnitureController.java
+++ trunk/FurnitureLibraryEditor/src/com/eteks/furniturelibraryeditor/viewcontroller/ImportFurnitureController.java
@@ -23,50 +23,16 @@ import java.util.concurrent.Callable;
 
 import com.eteks.furniturelibraryeditor.model.FurnitureLibrary;
 import com.eteks.furniturelibraryeditor.model.FurnitureLibraryUserPreferences;
-import com.eteks.sweethome3d.model.BackgroundImage;
 import com.eteks.sweethome3d.model.CatalogPieceOfFurniture;
-import com.eteks.sweethome3d.model.CatalogTexture;
-import com.eteks.sweethome3d.model.FurnitureCatalog;
-import com.eteks.sweethome3d.model.Home;
 import com.eteks.sweethome3d.model.RecorderException;
 import com.eteks.sweethome3d.model.UserPreferences;
-import com.eteks.sweethome3d.viewcontroller.BackgroundImageWizardController;
-import com.eteks.sweethome3d.viewcontroller.CompassController;
 import com.eteks.sweethome3d.viewcontroller.ContentManager;
 import com.eteks.sweethome3d.viewcontroller.Controller;
-import com.eteks.sweethome3d.viewcontroller.DialogView;
-import com.eteks.sweethome3d.viewcontroller.FurnitureCatalogController;
-import com.eteks.sweethome3d.viewcontroller.FurnitureController;
-import com.eteks.sweethome3d.viewcontroller.HelpController;
-import com.eteks.sweethome3d.viewcontroller.HelpView;
-import com.eteks.sweethome3d.viewcontroller.Home3DAttributesController;
-import com.eteks.sweethome3d.viewcontroller.HomeController;
-import com.eteks.sweethome3d.viewcontroller.HomeController3D;
-import com.eteks.sweethome3d.viewcontroller.HomeFurnitureController;
-import com.eteks.sweethome3d.viewcontroller.HomeView;
-import com.eteks.sweethome3d.viewcontroller.ImportedFurnitureWizardController;
-import com.eteks.sweethome3d.viewcontroller.ImportedFurnitureWizardStepsView;
-import com.eteks.sweethome3d.viewcontroller.ImportedTextureWizardController;
-import com.eteks.sweethome3d.viewcontroller.LabelController;
-import com.eteks.sweethome3d.viewcontroller.LevelController;
-import com.eteks.sweethome3d.viewcontroller.ModelMaterialsController;
-import com.eteks.sweethome3d.viewcontroller.ObserverCameraController;
-import com.eteks.sweethome3d.viewcontroller.PageSetupController;
-import com.eteks.sweethome3d.viewcontroller.PhotoController;
-import com.eteks.sweethome3d.viewcontroller.PlanController;
-import com.eteks.sweethome3d.viewcontroller.PlanView;
-import com.eteks.sweethome3d.viewcontroller.PrintPreviewController;
-import com.eteks.sweethome3d.viewcontroller.RoomController;
-import com.eteks.sweethome3d.viewcontroller.TextureChoiceController;
-import com.eteks.sweethome3d.viewcontroller.TextureChoiceView;
 import com.eteks.sweethome3d.viewcontroller.ThreadedTaskController;
 import com.eteks.sweethome3d.viewcontroller.ThreadedTaskView;
-import com.eteks.sweethome3d.viewcontroller.UserPreferencesController;
-import com.eteks.sweethome3d.viewcontroller.VideoController;
 import com.eteks.sweethome3d.viewcontroller.View;
 import com.eteks.sweethome3d.viewcontroller.ViewFactory;
-import com.eteks.sweethome3d.viewcontroller.WallController;
-import com.eteks.sweethome3d.viewcontroller.WizardController;
+import com.eteks.sweethome3d.viewcontroller.ViewFactoryAdapter;
 
 /**
  * Controller used to import furniture.
@@ -115,151 +81,10 @@ public class ImportFurnitureController implements Controller {
         }
       };
    
-    ViewFactory threadedTaskViewFactory = new ViewFactory() {
+    ViewFactory threadedTaskViewFactory = new ViewFactoryAdapter() {
         public ThreadedTaskView createThreadedTaskView(String taskMessage, UserPreferences preferences,
                                                        ThreadedTaskController controller) {
           return editorViewFactory.createImportFurnitureView(taskMessage, (FurnitureLibraryUserPreferences)preferences, controller);
-        }
-
-        public View createBackgroundImageWizardStepsView(BackgroundImage backgroundImage,
-                                                         UserPreferences preferences,
-                                                         BackgroundImageWizardController backgroundImageWizardController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public View createFurnitureCatalogView(FurnitureCatalog catalog,
-                                               UserPreferences preferences,
-                                               FurnitureCatalogController furnitureCatalogController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public View createFurnitureView(Home home,
-                                        UserPreferences preferences,
-                                        FurnitureController furnitureController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public HelpView createHelpView(UserPreferences preferences,
-                                       HelpController helpController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public DialogView createHome3DAttributesView(UserPreferences preferences,
-                                                     Home3DAttributesController home3dAttributesController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public DialogView createLevelView(UserPreferences preferences,
-                                          LevelController levelController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public DialogView createHomeFurnitureView(UserPreferences preferences,
-                                                  HomeFurnitureController homeFurnitureController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public HomeView createHomeView(Home home,
-                                       UserPreferences preferences,
-                                       HomeController homeController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public ImportedFurnitureWizardStepsView createImportedFurnitureWizardStepsView(CatalogPieceOfFurniture piece,
-                                                                                       String modelName,
-                                                                                       boolean importHomePiece,
-                                                                                       UserPreferences preferences,
-                                                                                       ImportedFurnitureWizardController importedFurnitureWizardController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public View createImportedTextureWizardStepsView(CatalogTexture texture,
-                                                         String textureName,
-                                                         UserPreferences preferences,
-                                                         ImportedTextureWizardController importedTextureWizardController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public DialogView createLabelView(boolean modification,
-                                          UserPreferences preferences,
-                                          LabelController labelController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public DialogView createPageSetupView(UserPreferences preferences,
-                                              PageSetupController pageSetupController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public DialogView createPhotoView(Home home,
-                                          UserPreferences preferences,
-                                          PhotoController photoController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public PlanView createPlanView(Home home,
-                                       UserPreferences preferences,
-                                       PlanController planController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public DialogView createPrintPreviewView(Home home,
-                                                 UserPreferences preferences,
-                                                 HomeController homeController,
-                                                 PrintPreviewController printPreviewController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public DialogView createRoomView(UserPreferences preferences,
-                                         RoomController roomController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public TextureChoiceView createTextureChoiceView(UserPreferences preferences,
-                                                         TextureChoiceController textureChoiceController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public View createModelMaterialsView(UserPreferences preferences,
-                                             ModelMaterialsController modelMaterialsController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public DialogView createUserPreferencesView(UserPreferences preferences,
-                                                    UserPreferencesController userPreferencesController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public DialogView createVideoView(Home home,
-                                          UserPreferences preferences,
-                                          VideoController videoController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public View createView3D(Home home,
-                                 UserPreferences preferences,
-                                 HomeController3D homeController3D) {
-          throw new UnsupportedOperationException();
-        }
-
-        public DialogView createWallView(UserPreferences preferences,
-                                         WallController wallController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public DialogView createWizardView(UserPreferences preferences,
-                                           WizardController wizardController) {
-          throw new UnsupportedOperationException();
-        }
-        
-        public DialogView createCompassView(UserPreferences preferences,
-                                            CompassController compassController) {
-          throw new UnsupportedOperationException();
-        }
-
-        public DialogView createObserverCameraView(UserPreferences preferences,
-                                                   ObserverCameraController home3dAttributesController) {
-          throw new UnsupportedOperationException();
         }
       };
 
