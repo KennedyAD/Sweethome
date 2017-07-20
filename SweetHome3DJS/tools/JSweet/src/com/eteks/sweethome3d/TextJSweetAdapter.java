828eb1d6810baa482101692ad51495d67f076c4a
nur <nur@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2017-07-20 15:56:56 +0000

    Added support for String.format (uses external lib sprintf.js).

--- SweetHome3DJS/tools/JSweet/src/com/eteks/sweethome3d/TextJSweetAdapter.java
+++ SweetHome3DJS/tools/JSweet/src/com/eteks/sweethome3d/TextJSweetAdapter.java
@@ -1,5 +1,5 @@
 /*
- * SweetHome3DJSweetFactory.java 
+ * SweetHome3DJava3DJSweetAdapter.java 
  *
  * Sweet Home 3D, Copyright (c) 2017 Emmanuel PUYBARET / eTeks <info@eteks.com>
  *
@@ -19,25 +19,35 @@
  */
 package com.eteks.sweethome3d;
 
-import org.jsweet.transpiler.JSweetContext;
-import org.jsweet.transpiler.JSweetFactory;
+import java.text.Format;
+
 import org.jsweet.transpiler.extension.PrinterAdapter;
+import org.jsweet.transpiler.model.MethodInvocationElement;
 
 /**
- * JSweet extension to generate JavaScript code from the SweetHome3D Java code
- * base.
+ * This adapter tunes the JavaScript generation to use the sprintf JavaScript
+ * library for String.format.
+ * 
+ * <p>
+ * It requires that https://github.com/alexei/sprintf.js is available.
  * 
  * @author Renaud Pawlak
  */
-public class SweetHome3DJSweetFactory extends JSweetFactory {
+public class TextJSweetAdapter extends PrinterAdapter {
 
-  public PrinterAdapter createAdapter(JSweetContext context) {
-    return new TextJSweetAdapter( //
-        new AWTJSweetAdapter( //
-            new SAXParserJSweetAdapter( //
-                new SweetHome3DJava3DJSweetAdapter( //
-                    new SweetHome3DJSweetAdapter( //
-                        super.createAdapter(context))))));
+  public TextJSweetAdapter(PrinterAdapter parent) {
+    super(parent);
+    addTypeMapping(Format.class.getName(), "string");
+  }
+
+  @Override
+  public boolean substituteMethodInvocation(MethodInvocationElement invocation) {
+    if (invocation.getMethodName().equals("format")
+        && String.class.getName().equals(invocation.getTargetExpression().getTypeAsElement().toString())) {
+      print("window['sprintf'](").printArgList(invocation.getArguments()).print(")");
+      return true;
+    }
+    return super.substituteMethodInvocation(invocation);
   }
 
 }
