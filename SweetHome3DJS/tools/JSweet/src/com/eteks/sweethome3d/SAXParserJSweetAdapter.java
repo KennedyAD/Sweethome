3aa8589af66e5ef49fee2ddac58995514bd6cc4a
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2017-07-27 16:38:12 +0000

    Fixed issue with entity management in jsXmlSaxParser

--- SweetHome3DJS/tools/JSweet/src/com/eteks/sweethome3d/SAXParserJSweetAdapter.java
+++ SweetHome3DJS/tools/JSweet/src/com/eteks/sweethome3d/SAXParserJSweetAdapter.java
@@ -1,5 +1,5 @@
 /*
- * SweetHome3DJava3DJSweetAdapter.java 
+ * SAXParserJSweetAdapter.java 
  *
  * Sweet Home 3D, Copyright (c) 2017 Emmanuel PUYBARET / eTeks <info@eteks.com>
  *
@@ -19,17 +19,18 @@
  */
 package com.eteks.sweethome3d;
 
+import javax.lang.model.element.Element;
+
 import org.jsweet.transpiler.extension.PrinterAdapter;
+import org.jsweet.transpiler.model.MethodInvocationElement;
 import org.jsweet.transpiler.model.NewClassElement;
 
 /**
  * This adapter tunes the JavaScript generation adapting Java SAX parsers to a
  * JavaScript library.
- * 
  * @author Renaud Pawlak
  */
 public class SAXParserJSweetAdapter extends PrinterAdapter {
-
   public SAXParserJSweetAdapter(PrinterAdapter parent) {
     super(parent);
 
@@ -37,16 +38,38 @@ public class SAXParserJSweetAdapter extends PrinterAdapter {
     addTypeMapping("org.xml.sax.SAXException", "SAXException");
     addTypeMapping("org.xml.sax.Attributes", "Attributes");
     
+    // Fix jsXmlSaxParser which doesn't replace quot lt gt amp entities in parsed text
+    addAnnotation(
+        "@Replace('return this.buffer.str.replace(/&quot;/g, '\\\"').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&amp;/g, '&');')",
+        "com.eteks.sweethome3d.io.HomeXMLHandler.getCharacters()");
   }
 
   @Override
   public boolean substituteNewClass(NewClassElement newClass) {
     switch (newClass.getTypeAsElement().toString()) {
-    case "org.xml.sax.SAXException":
-      print("new SAXException(").printArgList(newClass.getArguments()).print(")");
-      return true;
+      case "org.xml.sax.SAXException":
+        print("new SAXException(").printArgList(newClass.getArguments()).print(")");
+        return true;
     }
     return super.substituteNewClass(newClass);
   }
-  
+
+
+  @Override
+  public boolean substituteMethodInvocation(MethodInvocationElement invocation) {
+    if (invocation.getTargetExpression() != null) {
+      Element targetType = invocation.getTargetExpression().getTypeAsElement();
+      switch (targetType.toString()) {
+        case "org.xml.sax.Attributes":
+          if ("getValue".equals(invocation.getMethodName())) {
+            // Fix jsXmlSaxParser which doesn't replace quot lt gt amp entities in parsed text
+            print(invocation.getTargetExpression()).print(".getValue(").print(invocation.getArguments().get(0)).
+                print(").replace(\"&quot;\", \"\\\"\").replace(\"&lt;\", \"<\").replace(\"&gt;\", \">\").replace(\"&amp;\", \"&\")");
+            return true;
+          }
+          break;
+      }
+    }
+    return super.substituteMethodInvocation(invocation);
+  }
 }
