39213a4e08c9de681fe95656c3f39a9864f385b9
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2017-07-28 12:12:25 +0000

    Moved adapters to com.eteks.sweethome3d.jsweet

--- trunk/SweetHome3DJS/tools/JSweet/src/com/eteks/sweethome3d/jsweet/SweetHome3DJSweetFactory.java
+++ trunk/SweetHome3DJS/tools/JSweet/src/com/eteks/sweethome3d/jsweet/SweetHome3DJSweetFactory.java
@@ -13,7 +13,7 @@
  * You should have received a copy of the GNU General Public License along with this program; if not, write to the Free
  * Software Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
  */
-package com.eteks.sweethome3d;
+package com.eteks.sweethome3d.jsweet;
 
 import org.jsweet.transpiler.JSweetContext;
 import org.jsweet.transpiler.JSweetFactory;
