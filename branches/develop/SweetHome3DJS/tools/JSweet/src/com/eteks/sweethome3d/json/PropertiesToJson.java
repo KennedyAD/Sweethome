7c73838659f405d5ae47bc126eaa50e2055c7c9f
nur <nur@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2020-04-14 12:57:23 +0000

    Upgraded to latest JSweet version + json tooling

---
+++ branches/develop/SweetHome3DJS/tools/JSweet/src/com/eteks/sweethome3d/json/PropertiesToJson.java
@@ -0,0 +1,54 @@
+package com.eteks.sweethome3d.json;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.Properties;
+
+import org.json.JSONObject;
+
+public class PropertiesToJson {
+
+  public static void main(String[] args) throws Exception {
+    String[] sourcePropertyDirectories = { "../SweetHome3D/src/com/eteks/sweethome3d",
+        "../SweetHome3D/src/com/eteks/sweethome3d/applet", "../SweetHome3D/src/com/eteks/sweethome3d/swing",
+        "../SweetHome3D/src/com/eteks/sweethome3d/viewcontroller" };
+    String[] supportedLanguages = { "", "_bg", "_cs", "_de", "_el", "_en", "_es", "_fr", "_it", "_ja", "_hu", "_nl",
+        "_pl", "_pt", "_ru", "_sv", "_vi", "_zh_CN", "_zh_TW" };
+    String outputDirectory = "tools/json/generated";
+    String outputName = "localization";
+    new PropertiesToJson().convert(sourcePropertyDirectories, "package", outputDirectory, outputName,
+        supportedLanguages);
+  }
+
+  public void convert(String[] sourcePropertyDirectories, String sourceName, String outputDirectory, String outputName,
+      String[] supportedLanguages) throws Exception {
+
+    new File(outputDirectory).mkdirs();
+    for (String lang : supportedLanguages) {
+      Path outputFilePath = Paths.get(outputDirectory, outputName + lang + ".json");
+      System.out.println("Generating " + outputFilePath + "...");
+      outputFilePath.toFile().delete();
+      outputFilePath.toFile().createNewFile();
+      Properties properties = new Properties();
+      for (String sourcePropertyDirectory : sourcePropertyDirectories) {
+        File dir = new File(sourcePropertyDirectory);
+        for (File file : dir.listFiles()) {
+          if (file.getName().equals(sourceName + lang + ".properties")) {
+            Properties props = new Properties();
+            props.load(new FileInputStream(file));
+            System.out.println("Loading " + props.size() + " properties from " + file + ".");
+            properties.putAll(props);
+          }
+        }
+      }
+      System.out.println("Writing " + properties.size() + " properties to " + outputFilePath + ".");
+      Files.write(outputFilePath, (new JSONObject(properties).toString() + "\n").getBytes(), StandardOpenOption.APPEND);
+    }
+
+  }
+
+}
