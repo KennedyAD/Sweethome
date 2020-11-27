7cea821a92df8fbd130eff16a570bc0f8c385d2a
nur <nur@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2020-11-27 09:52:57 +0000

    Reorganized code and avoided passing useless view factories

--- SweetHome3DJS/src/JSViewFactory.js
+++ SweetHome3DJS/src/JSViewFactory.js
@@ -87,7 +87,7 @@ JSViewFactory.prototype.createView3D = function(home, preferences, homeControlle
 }
 
 JSViewFactory.prototype.createHomeView = function(home, preferences, homeController) {
-  return new HomePane("home-pane", home, preferences, homeController, this);
+  return new HomePane("home-pane", home, preferences, homeController);
 }
 
 JSViewFactory.prototype.createWizardView = function(preferences, wizardController) {
