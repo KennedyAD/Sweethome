792937c351f4a55888ee9a10ee9a3d85084e8935
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2021-01-22 17:41:40 +0000

    Added contentMatchingFurnitureName and furnitureIdEditable editable preferences

--- trunk/FurnitureLibraryEditor/src/com/eteks/furniturelibraryeditor/model/FurnitureLibraryUserPreferences.java
+++ trunk/FurnitureLibraryEditor/src/com/eteks/furniturelibraryeditor/model/FurnitureLibraryUserPreferences.java
@@ -19,6 +19,7 @@
  */
 package com.eteks.furniturelibraryeditor.model;
 
+import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 import com.eteks.sweethome3d.model.LengthUnit;
@@ -30,18 +31,20 @@ import com.eteks.sweethome3d.model.UserPreferences;
  */
 public abstract class FurnitureLibraryUserPreferences extends UserPreferences {
   /**
-   * The properties of user preferences that may change. <code>PropertyChangeListener</code>s added 
+   * The properties of user preferences that may change. <code>PropertyChangeListener</code>s added
    * to user preferences will be notified under a property name equal to the string value of one these properties.
    */
   public enum Property {DEFAULT_CREATOR, OFFLINE_FURNITURE_LIBRARY, FURNITURE_RESOURCES_LOCAL_DIRECTORY,
-                        FURNITURE_RESOURCES_REMOTE_URL_BASE}
-  
+                        FURNITURE_RESOURCES_REMOTE_URL_BASE, FURNITURE_ID_EDITABLE, CONTENT_MATCHING_FURNITURE_NAME}
+
   private final PropertyChangeSupport propertyChangeSupport;
   private String [] editedProperties;
   private String    defaultCreator;
   private boolean   offlineFurnitureLibrary;
   private String    furnitureResourcesLocalDirectory;
   private String    furnitureResourcesRemoteUrlBase;
+  private boolean   furnitureIdEditable;
+  private boolean   contentMatchingFurnitureName;
 
   public FurnitureLibraryUserPreferences() {
     this.propertyChangeSupport = new PropertyChangeSupport(this);
@@ -63,13 +66,50 @@ public abstract class FurnitureLibraryUserPreferences extends UserPreferences {
         FurnitureLibrary.FURNITURE_MODEL_ROTATION_PROPERTY};
     setUnit(LengthUnit.CENTIMETER);
     this.offlineFurnitureLibrary = true;
+    this.contentMatchingFurnitureName = true;
   }
 
   @Override
   public String [] getSupportedLanguages() {
     return new String [] {"en", "fr"};
   }
-  
+
+  /**
+   * Adds the property change <code>listener</code> in parameter to these preferences.
+   */
+  public void addPropertyChangeListener(PropertyChangeListener listener) {
+    super.addPropertyChangeListener(listener);
+    this.propertyChangeSupport.addPropertyChangeListener(listener);
+  }
+
+  /**
+   * Removes the property change <code>listener</code> in parameter from these preferences.
+   */
+  public void removePropertyChangeListener(PropertyChangeListener listener) {
+    this.propertyChangeSupport.removePropertyChangeListener(listener);
+  }
+
+  /**
+   * Adds the <code>listener</code> in parameter to these preferences to listen
+   * to the changes of the given <code>property</code>.
+   * <br>Caution: a user preferences instance generally exists during all the application ;
+   * therefore you should take care of not bounding permanently listeners to this
+   * object (for example, do not create anonymous listeners on user preferences
+   * in classes depending on an edited home).
+   */
+  public void addPropertyChangeListener(Property property,
+                                        PropertyChangeListener listener) {
+    this.propertyChangeSupport.addPropertyChangeListener(property.name(), listener);
+  }
+
+  /**
+   * Removes the <code>listener</code> in parameter from these preferences.
+   */
+  public void removePropertyChangeListener(Property property,
+                                           PropertyChangeListener listener) {
+    this.propertyChangeSupport.removePropertyChangeListener(property.name(), listener);
+  }
+
   /**
    * Returns the language used to retrieve the default localized values of the furniture.
    */
@@ -83,22 +123,22 @@ public abstract class FurnitureLibraryUserPreferences extends UserPreferences {
   public String [] getEditedProperties() {
     return this.editedProperties;
   }
-  
+
   /**
-   * Returns <code>true</code> if model content should always converted to OBJ format 
-   * at importation time. 
+   * Returns <code>true</code> if model content should always converted to OBJ format
+   * at importation time.
    */
   public boolean isModelContentAlwaysConvertedToOBJFormat() {
     return false;
   }
-  
+
   /**
    * Returns the creator used by default for imported furniture or <code>null</code>.
    */
   public String getDefaultCreator() {
     return this.defaultCreator;
   }
-  
+
   /**
    * Sets the creator used by default for imported furniture.
    */
@@ -107,28 +147,28 @@ public abstract class FurnitureLibraryUserPreferences extends UserPreferences {
         || defaultCreator != null && !defaultCreator.equals(this.defaultCreator)) {
       String oldDefaultCreator = this.defaultCreator;
       this.defaultCreator = defaultCreator;
-      this.propertyChangeSupport.firePropertyChange(Property.DEFAULT_CREATOR.toString(), oldDefaultCreator, defaultCreator);
+      this.propertyChangeSupport.firePropertyChange(Property.DEFAULT_CREATOR.name(), oldDefaultCreator, defaultCreator);
     }
   }
 
   /**
    * Returns <code>true</code> if the user may edit online libraries,
-   * and sets furniture resources local directory and remote URL base.   
+   * and sets furniture resources local directory and remote URL base.
    */
   public boolean isOnlineFurnitureLibrarySupported() {
     return false;
   }
-  
+
   /**
-   * Returns <code>true</code> if resources needed by the furniture of a library 
-   * must be included with the library to let it work without connection. 
+   * Returns <code>true</code> if resources needed by the furniture of a library
+   * must be included with the library to let it work without connection.
    */
   public boolean isFurnitureLibraryOffline() {
     return this.offlineFurnitureLibrary;
   }
-  
+
   /**
-   * Sets whether resources needed by the furniture of a library 
+   * Sets whether resources needed by the furniture of a library
    * must be included with the library to let it work without connection or not.
    */
   public void setFurnitureLibraryOffline(boolean offlineFurnitureLibrary) {
@@ -137,7 +177,7 @@ public abstract class FurnitureLibraryUserPreferences extends UserPreferences {
     }
     if (offlineFurnitureLibrary != this.offlineFurnitureLibrary) {
       this.offlineFurnitureLibrary = offlineFurnitureLibrary;
-      this.propertyChangeSupport.firePropertyChange(Property.OFFLINE_FURNITURE_LIBRARY.toString(), 
+      this.propertyChangeSupport.firePropertyChange(Property.OFFLINE_FURNITURE_LIBRARY.name(),
           !offlineFurnitureLibrary, offlineFurnitureLibrary);
     }
   }
@@ -149,7 +189,7 @@ public abstract class FurnitureLibraryUserPreferences extends UserPreferences {
   public String getFurnitureResourcesLocalDirectory() {
     return this.furnitureResourcesLocalDirectory;
   }
-  
+
   /**
    * Sets the local directory where resources needed by the furniture of a library
    * will be saved before being deployed on server.
@@ -162,23 +202,23 @@ public abstract class FurnitureLibraryUserPreferences extends UserPreferences {
         || furnitureResourcesLocalDirectory != null && !furnitureResourcesLocalDirectory.equals(this.furnitureResourcesLocalDirectory)) {
       String oldValue = this.furnitureResourcesLocalDirectory;
       this.furnitureResourcesLocalDirectory = furnitureResourcesLocalDirectory;
-      this.propertyChangeSupport.firePropertyChange(Property.FURNITURE_RESOURCES_LOCAL_DIRECTORY.toString(),
+      this.propertyChangeSupport.firePropertyChange(Property.FURNITURE_RESOURCES_LOCAL_DIRECTORY.name(),
           oldValue, furnitureResourcesLocalDirectory);
     }
   }
 
   /**
-   * Returns the URL base (relative or absolute) used to build the path to resources 
-   * needed by the furniture of a library. 
+   * Returns the URL base (relative or absolute) used to build the path to resources
+   * needed by the furniture of a library.
    */
   public String getFurnitureResourcesRemoteURLBase() {
     return this.furnitureResourcesRemoteUrlBase;
   }
-  
+
   /**
-   * Sets the URL base (relative or absolute) used to build the path to resources 
+   * Sets the URL base (relative or absolute) used to build the path to resources
    * needed by the furniture of a library. This base should be ended by a / character
-   * if it's a directory. 
+   * if it's a directory.
    */
   public void setFurnitureResourcesRemoteURLBase(String furnitureResourcesRemoteUrlBase) {
     if (!isOnlineFurnitureLibrarySupported()) {
@@ -188,19 +228,47 @@ public abstract class FurnitureLibraryUserPreferences extends UserPreferences {
         || furnitureResourcesRemoteUrlBase != null && !furnitureResourcesRemoteUrlBase.equals(this.furnitureResourcesRemoteUrlBase)) {
       Object oldValue = this.furnitureResourcesRemoteUrlBase;
       this.furnitureResourcesRemoteUrlBase = furnitureResourcesRemoteUrlBase;
-      this.propertyChangeSupport.firePropertyChange(Property.FURNITURE_RESOURCES_REMOTE_URL_BASE.toString(),
+      this.propertyChangeSupport.firePropertyChange(Property.FURNITURE_RESOURCES_REMOTE_URL_BASE.name(),
           oldValue, furnitureResourcesRemoteUrlBase);
     }
   }
 
   /**
-   * Returns <code>true</code> if the furniture content saved with the library should be named 
+   * Returns <code>true</code> if furniture ids are editable.
+   */
+  public boolean isFurnitureIdEditable() {
+    return this.furnitureIdEditable;
+  }
+
+  /**
+   * Sets whether furniture ids are editable.
+   */
+  public void setFurnitureIdEditable(boolean furnitureIdEditable) {
+    if (furnitureIdEditable != this.furnitureIdEditable) {
+      this.furnitureIdEditable = furnitureIdEditable;
+      this.propertyChangeSupport.firePropertyChange(Property.FURNITURE_ID_EDITABLE.name(), !furnitureIdEditable, furnitureIdEditable);
+    }
+  }
+
+  /**
+   * Returns <code>true</code> if the furniture content saved with the library should be named
    * from the furniture name in the default language.
    */
   public boolean isContentMatchingFurnitureName() {
-    return true;
+    return this.contentMatchingFurnitureName;
   }
-  
+
+  /**
+   * Sets whether the furniture content saved with the library should be named
+   * from the furniture name in the default language.
+   */
+  public void setContentMatchingFurnitureName(boolean contentMatchingFurnitureName) {
+    if (contentMatchingFurnitureName != this.contentMatchingFurnitureName) {
+      this.contentMatchingFurnitureName = contentMatchingFurnitureName;
+      this.propertyChangeSupport.firePropertyChange(Property.CONTENT_MATCHING_FURNITURE_NAME.name(), !contentMatchingFurnitureName, contentMatchingFurnitureName);
+    }
+  }
+
   /**
    * Returns <code>false</code>.
    */
