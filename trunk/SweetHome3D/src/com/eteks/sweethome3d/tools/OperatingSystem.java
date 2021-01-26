2052a28756ddb1604152935146f2ebd8a2e2dde7
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2021-01-26 15:12:42 +0000

    Added test for Big Sur

--- trunk/SweetHome3D/src/com/eteks/sweethome3d/tools/OperatingSystem.java
+++ trunk/SweetHome3D/src/com/eteks/sweethome3d/tools/OperatingSystem.java
@@ -142,6 +142,14 @@ public class OperatingSystem {
   }
 
   /**
+   * Returns <code>true</code> if current operating is Mac OS X 10.16 or superior.
+   * @since 6.5
+   */
+  public static boolean isMacOSXBigSurOrSuperior() {
+    return isMacOSX()
+        && compareVersions(System.getProperty("os.version"), "10.16") >= 0;
+  }
+  /**
    * Returns <code>true</code> if the given version is greater than or equal to the version
    * of the current JVM.
    * @since 4.0
