7cf62a19c5d8bd72b4a5a892140f460d2ea4bf32
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2021-10-26 19:22:40 +0000

    Improved recent colors management

--- SweetHome3DJS/src/colorSelection.js
+++ SweetHome3DJS/src/colorSelection.js
@@ -26,6 +26,7 @@
  * @param {HTMLElement} targetNode target node on which attach this component 
  * @constructor
  * @author Louis Grignon
+ * @author Emmanuel Puybaret
  * @private
  */
 function JSColorSelector(preferences, targetNode) {
@@ -34,10 +35,9 @@ function JSColorSelector(preferences, targetNode) {
       '<hr />' +
       '<div class="custom-color-editor">' +
       '  <input type="text" value="FFFFFF" />' +
-      '  <div class="preview"></div>' +
-      '  <button>➕</button>' +
+      '  <div class="preview"></div><div class="preview current-color"></div>' +
       '</div>' +
-      '<div class="custom-colors"></div>';
+      '<div class="recent-colors"></div>';
 
   JSComponent.call(this, preferences, html);
   if (targetNode != null) {
@@ -46,10 +46,11 @@ function JSColorSelector(preferences, targetNode) {
 
   this.getHTMLElement().classList.add("color-selector");
   this.pickerElement = this.getHTMLElement().querySelector(".picker");
-  this.customColorsContainerElement = this.getHTMLElement().querySelector(".custom-colors");
+  this.recentColorsContainerElement = this.getHTMLElement().querySelector(".recent-colors");
   this.customColorEditorInput = this.getHTMLElement().querySelector(".custom-color-editor input");
-  this.customColorEditorOverview = this.getHTMLElement().querySelector(".custom-color-editor .preview");
-  this.customColorEditorAddButton = this.getHTMLElement().querySelector(".custom-color-editor button");
+  var colorPreviews = this.getHTMLElement().querySelectorAll(".custom-color-editor .preview");
+  this.customColorEditorPreview = colorPreviews[0];
+  this.currentColorEditorPreview = colorPreviews[1];
 
   this.createPickerColorTiles();
   this.initCustomColorEditor();
@@ -63,19 +64,34 @@ JSColorSelector.prototype.constructor = JSColorSelector;
 JSColorSelector.prototype.createPickerColorTiles = function() {
   var colors = [
     // RAL colors from http://www.ralcolor.com/
-    "#BEBD7F", "#C2B078", "#C6A664", "#E5BE01", "#CDA434", "#A98307", "#E4A010", "#DC9D00", "#8A6642", "#C7B446", "#EAE6CA", "#E1CC4F", "#E6D690", "#EDFF21", "#F5D033", "#F8F32B", "#9E9764", "#999950", "#F3DA0B", "#FAD201", "#AEA04B", "#FFFF00", "#9D9101", "#F4A900", "#D6AE01", "#F3A505", "#EFA94A", "#6A5D4D", "#705335", "#F39F18", "#ED760E", "#C93C20", "#CB2821", "#FF7514", "#F44611", "#FF2301", "#FFA420", "#F75E25", "#F54021", "#D84B20", "#EC7C26", "#E55137", "#C35831", "#AF2B1E", "#A52019", "#A2231D", "#9B111E", "#75151E", "#5E2129", "#412227", "#642424", "#781F19", "#C1876B", "#A12312", "#D36E70", "#EA899A", "#B32821", "#E63244", "#D53032", "#CC0605", "#D95030", "#F80000", "#FE0000", "#CB3234", "#B32428", "#721422", "#B44C43", "#6D3F5B", "#922B3E", "#DE4C8A", "#641C34", "#6C4675", "#A03472", "#4A192C", "#924E7D", "#A18594", "#CF3476", "#8673A1", "#6C6874", "#354D73", "#1F3438", "#20214F", "#1D1E33", "#18171C", "#1E2460", "#3E5F8A", "#26252D", "#025669", "#0E294B", "#231A24", "#3B83BD", "#1E213D", "#606E8C", "#2271B3", "#063971", "#3F888F", "#1B5583", "#1D334A", "#256D7B", "#252850", "#49678D", "#5D9B9B", "#2A6478", "#102C54", "#316650", "#287233", "#2D572C", "#424632", "#1F3A3D", "#2F4538", "#3E3B32", "#343B29", "#39352A", "#31372B", "#35682D", "#587246", "#343E40", "#6C7156", "#47402E", "#3B3C36", "#1E5945", "#4C9141", "#57A639", "#BDECB6", "#2E3A23", "#89AC76", "#25221B", "#308446", "#3D642D", "#015D52", "#84C3BE", "#2C5545", "#20603D", "#317F43", "#497E76", "#7FB5B5", "#1C542D", "#193737", "#008F39", "#00BB2D", "#78858B", "#8A9597", "#7E7B52", "#6C7059", "#969992", "#646B63", "#6D6552", "#6A5F31", "#4D5645", "#4C514A", "#434B4D", "#4E5754", "#464531", "#434750", "#293133", "#23282B", "#332F2C", "#686C5E", "#474A51", "#2F353B", "#8B8C7A", "#474B4E", "#B8B799", "#7D8471", "#8F8B66", "#D7D7D7", "#7F7679", "#7D7F7D", "#B5B8B1", "#6C6960", "#9DA1AA", "#8D948D", "#4E5452", "#CAC4B0", "#909090", "#82898F", "#D0D0D0", "#898176", "#826C34", "#955F20", "#6C3B2A", "#734222", "#8E402A", "#59351F", "#6F4F28", "#5B3A29", "#592321", "#382C1E", "#633A34", "#4C2F27", "#45322E", "#403A3A", "#212121", "#A65E2E", "#79553D", "#755C48", "#4E3B31", "#763C28", "#FDF4E3", "#E7EBDA", "#F4F4F4", "#282828", "#0A0A0A", "#A5A5A5", "#8F8F8F", "#FFFFFF", "#1C1C1C", "#F6F6F6", "#1E1E1E", "#D7D7D7", "#9C9C9C", "#828282"
-    ];
+    "#BEBD7F", "#C2B078", "#C6A664", "#E5BE01", "#CDA434", "#A98307", "#E4A010", "#DC9D00", "#8A6642", "#C7B446", "#EAE6CA", "#E1CC4F", "#E6D690", "#EDFF21", 
+    "#F5D033", "#F8F32B", "#9E9764", "#999950", "#F3DA0B", "#FAD201", "#AEA04B", "#FFFF00", "#9D9101", "#F4A900", "#D6AE01", "#F3A505", "#EFA94A", "#6A5D4D", 
+    "#705335", "#F39F18", "#ED760E", "#C93C20", "#CB2821", "#FF7514", "#F44611", "#FF2301", "#FFA420", "#F75E25", "#F54021", "#D84B20", "#EC7C26", "#E55137", 
+    "#C35831", "#AF2B1E", "#A52019", "#A2231D", "#9B111E", "#75151E", "#5E2129", "#412227", "#642424", "#781F19", "#C1876B", "#A12312", "#D36E70", "#EA899A", 
+    "#B32821", "#E63244", "#D53032", "#CC0605", "#D95030", "#F80000", "#FE0000", "#CB3234", "#B32428", "#721422", "#B44C43", "#6D3F5B", "#922B3E", "#DE4C8A", 
+    "#641C34", "#6C4675", "#A03472", "#4A192C", "#924E7D", "#A18594", "#CF3476", "#8673A1", "#6C6874", "#354D73", "#1F3438", "#20214F", "#1D1E33", "#18171C", 
+    "#1E2460", "#3E5F8A", "#26252D", "#025669", "#0E294B", "#231A24", "#3B83BD", "#1E213D", "#606E8C", "#2271B3", "#063971", "#3F888F", "#1B5583", "#1D334A", 
+    "#256D7B", "#252850", "#49678D", "#5D9B9B", "#2A6478", "#102C54", "#316650", "#287233", "#2D572C", "#424632", "#1F3A3D", "#2F4538", "#3E3B32", "#343B29", 
+    "#39352A", "#31372B", "#35682D", "#587246", "#343E40", "#6C7156", "#47402E", "#3B3C36", "#1E5945", "#4C9141", "#57A639", "#BDECB6", "#2E3A23", "#89AC76", 
+    "#25221B", "#308446", "#3D642D", "#015D52", "#84C3BE", "#2C5545", "#20603D", "#317F43", "#497E76", "#7FB5B5", "#1C542D", "#193737", "#008F39", "#00BB2D", 
+    "#78858B", "#8A9597", "#7E7B52", "#6C7059", "#969992", "#646B63", "#6D6552", "#6A5F31", "#4D5645", "#4C514A", "#434B4D", "#4E5754", "#464531", "#434750", 
+    "#293133", "#23282B", "#332F2C", "#686C5E", "#474A51", "#2F353B", "#8B8C7A", "#474B4E", "#B8B799", "#7D8471", "#8F8B66", "#D7D7D7", "#7F7679", "#7D7F7D", 
+    "#B5B8B1", "#6C6960", "#9DA1AA", "#8D948D", "#4E5452", "#CAC4B0", "#909090", "#82898F", "#D0D0D0", "#898176", "#826C34", "#955F20", "#6C3B2A", "#734222", 
+    "#8E402A", "#59351F", "#6F4F28", "#5B3A29", "#592321", "#382C1E", "#633A34", "#4C2F27", "#45322E", "#403A3A", "#212121", "#A65E2E", "#79553D", "#755C48", 
+    "#4E3B31", "#763C28", "#FDF4E3", "#E7EBDA", "#F4F4F4", "#282828", "#0A0A0A", "#A5A5A5", "#8F8F8F", "#FFFFFF", "#1C1C1C", "#F6F6F6", "#1E1E1E", "#D7D7D7", 
+    "#9C9C9C", "#828282"];
   
-  this.colorTileElements = [];
+  this.colorTileElements = [this.currentColorEditorPreview];
   for (var i = 0; i < colors.length; i++) {
     var colorHex = colors[i];
     var tileElement = this.createColorTile(colorHex);
     this.pickerElement.appendChild(tileElement);
   }
-  for (var i = 0; i < this.preferences.getRecentColors().length; i++) {
-    var colorHex = ColorTools.integerToHexadecimalString(this.preferences.getRecentColors()[i]);
+  var recentColors = this.preferences.getRecentColors();
+  for (var i = 0; i < recentColors.length; i++) {
+    var colorHex = ColorTools.integerToHexadecimalString(recentColors[i]);
     var tileElement = this.createColorTile(colorHex);
-    this.customColorsContainerElement.appendChild(tileElement);
+    this.recentColorsContainerElement.appendChild(tileElement);
   }
   
   var colorSelector = this;
@@ -86,23 +102,12 @@ JSColorSelector.prototype.createPickerColorTiles = function() {
 
 JSColorSelector.prototype.initCustomColorEditor = function() {
   var colorSelector = this;
-  var changeColorEvent = "keyup";
-  this.registerEventListener(this.customColorEditorInput, changeColorEvent, function(ev) {
+  this.registerEventListener(this.customColorEditorInput, "input", function(ev) {
       var colorHex = '#' + colorSelector.customColorEditorInput.value;
       if (colorHex.match(/#[0-9a-fA-F]{3,6}/)) {
-        colorSelector.customColorEditorOverview.style.backgroundColor = colorHex;
-        colorSelector.customColorEditorAddButton.disabled = false;
-      } else {
-        colorSelector.customColorEditorAddButton.disabled = true;
+        colorSelector.customColorEditorPreview.style.backgroundColor = colorHex;
+        colorSelector.color = ColorTools.hexadecimalStringToInteger(colorHex);
       }
-    });
-  this.registerEventListener(this.customColorEditorAddButton, "click", function(ev) {
-        var colorHex = '#' + colorSelector.customColorEditorInput.value;
-        colorSelector.addAndSelectCustomColorTile(colorHex);
-        colorSelector.customColorEditorInput.value = "FFFFFF";
-        var event = document.createEvent("Event");
-        event.initEvent(changeColorEvent, false, false);
-        colorSelector.customColorEditorInput.dispatchEvent(event);
     });
 }
   
@@ -120,12 +125,17 @@ JSColorSelector.prototype.getColor = function() {
  */
 JSColorSelector.prototype.setColor = function(color) {
   this.color = color;
-  var matchingTile = this.getHTMLElement().querySelector("[data-color='" + (color != null ? color : 0xFFFFFF) + "']");
-  if (matchingTile == null) {
-    this.addAndSelectCustomColorTile(ColorTools.integerToHexadecimalString(color != null ? color : 0xFFFFFF));
-  } else {
+  var displayedColor = color != null ? color : 0xFFFFFF;
+  var matchingTile = this.getHTMLElement().querySelector("[data-color='" + displayedColor + "']");
+  if (matchingTile != null) {
     this.selectColorTile(matchingTile);
+  } else {
+    this.customColorEditorInput.value = ColorTools.integerToHexadecimalString(displayedColor).substring(1);
+    this.customColorEditorPreview.style.backgroundColor = ColorTools.integerToHexadecimalString(displayedColor);
   }
+  
+  this.currentColorEditorPreview.style.backgroundColor = ColorTools.integerToHexadecimalString(displayedColor);
+  this.currentColorEditorPreview.dataset["color"] = displayedColor;
 }
 
 /**
@@ -143,11 +153,11 @@ JSColorSelector.prototype.selectColorTile = function(tileElement) {
   }
   this.color = this.getTileColor(tileElement);
   this.customColorEditorInput.value = ColorTools.integerToHexadecimalString(this.color).substring(1);
-  this.customColorEditorOverview.style.backgroundColor = this.customColorEditorInput.value;
+  this.customColorEditorPreview.style.backgroundColor = this.customColorEditorInput.value;
 }
 
 /**
- * Returns color as ARGB number from tile element
+ * Returns color as ARGB number from tile element.
  * @param {HTMLElement} tileElement
  * @return color as ARGB number
  * @private
@@ -157,7 +167,7 @@ JSColorSelector.prototype.getTileColor = function(tileElement) {
 }
 
 /**
- * Creates a color tile element from hexadecimal color
+ * Creates a color tile element from hexadecimal color.
  * @param {string} colorHex hexadecimal color
  * @return {HTMLElement} tile element
  * @private
@@ -167,31 +177,6 @@ JSColorSelector.prototype.createColorTile = function(colorHex) {
   tileElement.dataset["color"] = ColorTools.hexadecimalStringToInteger(colorHex);
   tileElement.style.backgroundColor = colorHex;
   this.colorTileElements.push(tileElement);
-  return tileElement;
-}
-
-/**
- * Creates, add and select a new custom color tile for given hexadecimal color
- * @param {string} colorHex hexadecimal color
- * @return {HTMLElement} tile element
- * @private
- */
-JSColorSelector.prototype.addAndSelectCustomColorTile = function(colorHex) {
-  var colorSelector = this;
-  var tileElement = this.createColorTile(colorHex);
-  this.customColorsContainerElement.appendChild(tileElement);
-
-  var colorNumber = ColorTools.hexadecimalStringToInteger(colorHex);
-  if (this.preferences.getRecentColors().indexOf(colorNumber) === -1) {
-    var recentColors = [colorNumber].concat(this.preferences.getRecentColors());
-    this.preferences.setRecentColors(recentColors);
-  }
-
-  this.registerEventListener(tileElement, "click", function(ev) { 
-      colorSelector.selectColorTile(this); 
-    });
-  this.selectColorTile(tileElement);
-
   return tileElement;
 }
 
@@ -263,7 +248,7 @@ function JSColorSelectorButton(preferences, targetNode, options) {
   
   var component = this;
   this.registerEventListener(this.button, "click", function(ev) { 
-      component.openColorSelectorDialog();
+      component.openColorSelectorDialog(preferences);
     });
   
   this.colorOverview = this.getHTMLElement().querySelector(".color-preview");
@@ -312,14 +297,27 @@ JSColorSelectorButton.prototype.setEnabled = function(enabled) {
   this.button.disabled = !enabled;
 }
 
-JSColorSelectorButton.prototype.openColorSelectorDialog = function() {
+JSColorSelectorButton.prototype.openColorSelectorDialog = function(preferences) {
   var button = this;
   var dialog = new JSColorSelectorDialog(this.preferences, 
       { 
         color: this.color,
         title: this.colorDialogTitle,
         applier: function() {
-          button.setColor(dialog.getSelectedColor());
+          var color = dialog.getSelectedColor();
+          button.setColor(color);
+          var recentColors = preferences.getRecentColors().slice(0);
+          var colorIndex = recentColors.indexOf(color);
+          if (colorIndex != 0) {
+            // Move color at the beginning of the list and ensure it doesn't contain more than 14 colors
+            if (colorIndex > 0) {
+              recentColors.splice(colorIndex, 1);
+            } else {
+              recentColors.length = Math.min(recentColors.length, 14);
+            }
+            recentColors.splice(0, 0, color);
+            preferences.setRecentColors(recentColors);
+          }  
           if (typeof button.options.colorChanged == "function") {
             button.options.colorChanged(dialog.getSelectedColor());
           }
