eda4ebf600ff42ea06993152ec0553510acd951e
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2023-07-25 09:29:34 +0000

    Avoided to display empty additional properties table

--- trunk/SweetHome3D/src/com/eteks/sweethome3d/viewcontroller/HomeFurnitureController.java
+++ trunk/SweetHome3D/src/com/eteks/sweethome3d/viewcontroller/HomeFurnitureController.java
@@ -383,7 +383,7 @@ public class HomeFurnitureController implements Controller {
                    : firstPiece.getProperty(propertyName));
           }
         }
-        setAdditionalProperties(additionalProperties);
+        setAdditionalProperties(additionalProperties.size() > 0 ? additionalProperties : null);
       } else {
         setAdditionalProperties(null);
       }
