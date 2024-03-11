4bb705efb35bfd312d99e8e94bdaeeb0ad94b93b
puybaret <puybaret@03e323e1-6c6c-46a5-9f71-c4ce5d8c6204>
2024-03-11 13:10:04 +0000

    Fixed camera moves after double click

--- SweetHome3DJS/src/HomeComponent3D.js
+++ SweetHome3DJS/src/HomeComponent3D.js
@@ -1041,7 +1041,7 @@ HomeComponent3D.prototype.addMouseListeners = function(controller, preferences, 
       mouseDoubleClicked: function(ev) {
         mouseListener.updateCoordinates(ev, "mouseDoubleClicked");
         mouseListener.mousePressed(ev);
-        actionStartedInComponent3D = false;
+        mouseListener.actionStartedInComponent3D = false;
       },
       windowMouseMoved: function(ev) {
         if (!mouseListener.touchEventType
